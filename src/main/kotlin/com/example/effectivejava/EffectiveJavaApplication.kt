5ea42abe5fdb93d38bd9aca95dad6221a51438ce
ljw0829 <olo_jinwon@naver.com>
2022-10-24 23:11:41 +0900

    - feat : effective-java 초기 환경 세팅

---
+++ src/main/kotlin/com/example/effectivejava/EffectiveJavaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.effectivejava
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EffectiveJavaApplication
+
+fun main(args: Array<String>) {
+    runApplication<EffectiveJavaApplication>(*args)
+}
