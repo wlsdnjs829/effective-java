27f5a148061d8cc250ba41806701c46aeb5e1ec9
ljw0829 <olo_jinwon@naver.com>
2022-10-30 22:37:21 +0900

    - feat : 🔥 ITEM 18. 상속보다는 컴포지션을 사용하라   * 데코레이터 (래퍼 클래스) 코틀린으로 간단하게 해보자.

--- src/main/kotlin/com/example/effectivejava/three/ForwardingSet.kt
+++ src/main/kotlin/com/example/effectivejava/three/ForwardingSet.kt
@@ -1,29 +1,7 @@
 package com.example.effectivejava.three
 
-open class ForwardingSet<E>(private val mutableSet: MutableSet<E>) : MutableSet<E> {
+open class ForwardingSet<E>(private val mutableSet: MutableSet<E>) : MutableSet<E> by mutableSet {
 
     override val size: Int = 0
-
-    override fun clear() {
-        mutableSet.clear()
-    }
-
-    override fun isEmpty() = mutableSet.isEmpty()
-
-    override fun containsAll(elements: Collection<E>) = mutableSet.containsAll(elements)
-
-    override fun contains(element: E) = mutableSet.contains(element)
-
-    override fun iterator(): MutableIterator<E> = mutableSet.iterator()
-
-    override fun retainAll(elements: Collection<E>): Boolean = mutableSet.retainAll(elements.toSet())
-
-    override fun removeAll(elements: Collection<E>): Boolean = mutableSet.removeAll(elements.toSet())
-
-    override fun remove(element: E): Boolean = mutableSet.remove(element)
-
-    override fun addAll(elements: Collection<E>): Boolean = mutableSet.addAll(elements)
-
-    override fun add(element: E): Boolean = mutableSet.add(element)
 
 }
