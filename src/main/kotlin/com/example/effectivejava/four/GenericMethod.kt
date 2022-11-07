ac58e7096743790ab977c67f92ccd1a6e229dee1
ljw0829 <olo_jinwon@naver.com>
2022-11-07 21:09:29 +0900

    - feat : 🔥 ITEM 32. 제네릭과 가변인수를 함께 쓸 때는 신중하라

--- src/main/kotlin/com/example/effectivejava/four/GenericMethod.kt
+++ src/main/kotlin/com/example/effectivejava/four/GenericMethod.kt
@@ -1,5 +1,8 @@
 package com.example.effectivejava.four
 
+import com.example.effectivejava.four.GenericMethod.Companion.pickTwo
+import java.util.concurrent.ThreadLocalRandom
+
 class GenericMethod {
 
     companion object {
@@ -8,6 +11,18 @@ class GenericMethod {
             result.addAll(s2)
             return result
         }
+
+        fun <T> toArray(vararg args: T) = args
+
+        inline fun <reified T> pickTwo(a: T, b: T, c: T): Array<out T> {
+            return when (ThreadLocalRandom.current().nextInt(3)) {
+                0 -> toArray(a, b)
+                1 -> toArray(a, c)
+                2 -> toArray(b, c)
+                else -> emptyArray()
+            }
+        }
+
     }
 
 }
