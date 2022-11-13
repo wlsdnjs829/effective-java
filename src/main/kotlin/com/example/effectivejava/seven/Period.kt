1c5abd015a457d243960a0dce3ef7bd7bd9e77f3
ljw0829 <olo_jinwon@naver.com>
2022-11-13 14:57:06 +0900

    - feat : 🔥 ITEM 50. 적시에 방어적 복사본을 만들라

---
+++ src/main/kotlin/com/example/effectivejava/seven/Period.kt
@@ -0,0 +1,20 @@
+package com.example.effectivejava.seven
+
+import java.util.Date
+
+class Period(start: Date, end: Date) {
+
+    private val start: Date
+        get() = Date(field.time)
+
+    private val end: Date
+        get() = Date(field.time)
+
+    init {
+        this.start = Date(start.time)
+        this.end = Date(end.time)
+
+        assert(this.start > this.end) { println("${this.start}가 ${this.end}보다 늦다") }
+    }
+
+}
